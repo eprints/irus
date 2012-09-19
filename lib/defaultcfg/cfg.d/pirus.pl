10a7ed27626cefff1c933ce56ed86f82a68beb46
Tim Brody <tdb2@ecs.soton.ac.uk>
2012-09-19 11:05:32 +0100

    PIRUS extension extracted from epservices svn
    Added README and LICENSE (CC-0)
    
    Added note that 1.02 was created by Justin.

---
+++ lib/defaultcfg/cfg.d/pirus.pl
@@ -0,0 +1,107 @@
+=pod
+
+=head1 PIRUS 'PUSH' implementation
+
+Provide data for COUNTER-compliant usage statistics.
+
+Copyright 2012 University of Southampton
+
+Released to the public domain (or CC0 depending on your juristiction).
+
+USE OF THIS EXTENSION IS ENTIRELY AT YOUR OWN RISK
+
+=head2 Installation
+
+Copy this file into your repository's cfg.d/ directory and restart Apache.
+
+=head2 Implementation
+
+This code will PING the configured tracker server whenever a full-text object
+is requested from EPrints.
+
+These pings can be aggregated together with data from other sources
+(publishers, other repositories) to create a fuller picture of the usage of
+individual articles.
+
+
+The data transferred are:
+
+	- eprint.eprintid - the eprint's internal identifier
+	- eprint.datestamp - the datetime the access started
+	- IP address - the user's IP address
+	- User Agent - the user's browser user agent
+	- eprint.date - the publication date
+
+And either (if id_number is defined):
+
+	- eprint.id_number - the DOI
+
+Or:
+
+	- eprint.creators_name - first named author
+	- eprint.title - eprint title
+	- eprint.jtitle - publication title
+	- eprint.issn - publication ISSN
+	- eprint.volume - publication volume
+	- eprint.issue - publication issue
+
+=head2 Changes
+
+1.02 Justin Bradley <jb4@ecs.soton.ac.uk>
+
+Compatibility fixes for 3.2.
+
+1.01
+
+Fixed reference to 'jtitle' instead of 'publication'
+
+1.00
+
+Initial version
+
+=cut
+
+require LWP::UserAgent;
+require LWP::ConnCache;
+
+# modify the following URL to the PIRUS tracker location
+#$c->{pirus}->{tracker} = "http://cclibweb-4.dmz.cranfield.ac.uk/tracker/";
+$c->{pirus}->{tracker} = "http://www.jusp.mimas.ac.uk/counter/";
+
+# you may want to revise the settings for the user agent e.g. increase or
+# decrease the network timeout
+$c->{pirus}->{ua} = LWP::UserAgent->new(
+	from => $c->{adminemail},
+	agent => $c->{version},
+	timeout => 20,
+	conn_cache => LWP::ConnCache->new,
+);
+
+$c->{plugins}->{"Event::PIRUS"}->{params}->{disable} = 0;
+
+##############################################################################
+
+$c->add_dataset_trigger( 'access', EPrints::Const::EP_TRIGGER_CREATED, sub {
+	my( %args ) = @_;
+
+	my $repo = $args{repository};
+	my $access = $args{dataobj};
+
+	my $plugin = $repo->plugin( "Event::PIRUS" );
+
+	my $r = $plugin->log( $access );
+
+	if( defined $r && !$r->is_success )
+	{
+		my $event = $repo->dataset( "event_queue" )->dataobj_class->create_unique( $repo, {
+			eventqueueid => Digest::MD5::md5_hex( "Event::PIRUS::replay" ),
+			pluginid => "Event::PIRUS",
+			action => "replay",
+		});
+		if( defined $event )
+		{
+			$event->set_value( "params", [$access->id] );
+			$event->commit;
+		}
+	}
+});
---
+++ lib/plugins/EPrints/Plugin/Event/PIRUS.pm
@@ -0,0 +1,104 @@
+package EPrints::Plugin::Event::PIRUS;
+
+our $VERSION = v1.02;
+
+@ISA = qw( EPrints::Plugin::Event );
+
+use strict;
+
+# borrowed from EPrints 3.3's EPrints::OpenArchives::archive_id
+sub _archive_id
+{
+	my( $repo, $any ) = @_;
+
+	my $v1 = $repo->config( "oai", "archive_id" );
+	my $v2 = $repo->config( "oai", "v2", "archive_id" );
+
+	$v1 ||= $repo->config( "host" );
+	$v2 ||= $v1;
+
+	return $any ? ($v1, $v2) : $v2;
+}
+
+
+sub replay
+{
+	my( $self, $accessid ) = @_;
+
+	my $repo = $self->{session};
+
+	local $SIG{__DIE__};
+	eval { $repo->dataset( "access" )->search(filters => [
+				{ meta_fields => [qw( accessid )], value => "$accessid..", },
+				{ meta_fields => [qw( service_type_id )], value => "?fulltext=yes", match => "EX", },
+			],
+			limit => 1000, # lets not go crazy ...
+	)->map(sub {
+		(undef, undef, my $access) = @_;
+
+		my $r = $self->log( $access );
+		die "failed\n" if !$r->is_success;
+		$accessid = $access->id;
+	}) };
+	if( $@ eq "failed\n" )
+	{
+		$repo->log( "Attempt to re-send PIRUS trackback failed, trying again in 24 hours time" );
+
+		my $event = $self->{event};
+		$event->set_value( "params", [$accessid] );
+		$event->set_value( "start_time", EPrints::Time::iso_datetime( time + 86400 ) );
+		#return EPrints::Const::HTTP_RESET_CONTENT;
+		return 0;
+	}
+	elsif( $@ )
+	{
+		die $@;
+	}
+
+	return;
+}
+
+sub log
+{
+	my( $self, $access ) = @_;
+
+	my $repo = $self->{session};
+
+	return if $access->value( "service_type_id" ) ne "?fulltext=yes";
+
+	my $doc = $repo->dataset( "document" )->dataobj( $access->value( "referent_docid" ) );
+
+	my $url = URI->new(
+		$repo->config( "pirus", "tracker" )
+	);
+
+	my $url_tim = $access->value( "datestamp" );
+	$url_tim =~ s/^(\S+) (\S+)$/$1T$2Z/;
+
+	my $artnum = EPrints::OpenArchives::to_oai_identifier(
+		###	EPrints::OpenArchives::archive_id( $repo ),
+			_archive_id( $repo ),
+			$access->value( "referent_id" ),
+		);
+
+	### my $mime_type = $doc->exists_and_set( "mime_type" ) ?
+	###		$doc->value( "mime_type" ) :
+	###		$doc->value( "format" );
+	my $mime_type = $doc->value( "format" );
+
+	$url->query_form(
+		url_ver => "Z39.88-2004",
+		url_tim => $url_tim,
+		req_id => "urn:ip:".$access->value( "requester_id" ),
+		req_dat => $access->value( "requester_user_agent" ),
+		'rft.artnum' => $artnum,
+		svc_format => $mime_type,
+		rfr_id => $repo->config( "host" ),
+	);
+
+	my $ua = $repo->config( "pirus", "ua" );
+
+	return $ua->head( $url );
+}
+
+1;
